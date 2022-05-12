943592c70ff2bf8cebf7feec224ef1a619eea430
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-05-12 15:33:35 +0300

    made into class and oop

--- js/main.js
+++ js/main.js
@@ -1,194 +1,162 @@
 //Get the deck
 //localstorage and random deck id
-let deckId = ''
-if(!localStorage.getItem("credits")){
-  localStorage.setItem("credits", 1000)
-} else {
-}
 
-document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`
-document.querySelector("#addCredits").addEventListener("click", add1000);
-
-function add1000(){
-  let added = Number(localStorage.getItem("credits")) + 1000;
-  localStorage.setItem("credits", added);
+class Game{
+  constructor(credits, deck){
+    this.credits = credits
+    this.deck = deck
+    this.count = 0
+    this.player = 0
+    this.dealer = 0
+    this.bet = 100
+    this.deckId = ""
+  }
+ addcredits(){
+  this.credits = credits + 1000;
+  localStorage.setItem("credits", this.credits);
   document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`
-}
-let bet = 100;
-document.querySelector("#betSize").innerText = `Bet Size: ${bet}`;
-
-document.querySelector("#increase").addEventListener("click", increase)
-
-function increase(){
-  if(bet < localStorage.getItem("credits")){
-  bet += 100;
-  document.querySelector("#betSize").innerText = `Bet Size: ${bet}`;
+ }
+ 
+ increaseBet(){
+  if(this.bet < this.credits){
+    this.bet += 100;
+    document.querySelector("#betSize").innerText = `Bet Size: ${this.bet}`;
   }
-}
+ }
 
-document.querySelector("#decrease").addEventListener("click", decrease)
-function decrease(){
-  if(bet >= 100){
-  bet -= 100;
-  document.querySelector("#betSize").innerText = `Bet Size: ${bet}`;
+ decreaseBet(){
+  this.bet -= 100;
+  document.querySelector("#betSize").innerText = `Bet Size: ${this.bet}`;
+ }
+
+ renderCards(){
+  //fetch new Deck
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+    this.deckId = data.deck_id
+    })
+    .catch(err => {
+    console.log(`error ${err}`)
+  });
+setTimeout(() => {
+ let url = `https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=30`
+ fetch(url)
+     .then(res => res.json()) // parse response as JSON
+     .then(data => {
+       //setting cardValues into an array and access them later in the program
+       this.deck = data.cards;
+       console.log(this.deck);
+     })
+     .catch(err => {
+         console.log(`error ${err}`)
+     });
+
+  const dealer = document.getElementById("dealer-Cards");
+  const player = document.getElementById("player-Cards");
+  while(dealer.firstChild) {
+    dealer.removeChild(dealer.firstChild);
   }
-}
-
-
-
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        deckId = data.deck_id
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-
-
-document.querySelector('#dealCards').addEventListener('click', getFetch)
-
-
-function getFetch(){
+  while(player.firstChild) {
+  player.removeChild(player.firstChild);
+  }
   
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=20`
+  //setting the basic cards for player and dealer
+  while(this.count < 2){
+    let elem = document.createElement("img"); 
+    elem.src = this.deck[this.count].image
+    document.querySelector("#player-Cards").append(elem)
+    this.player += cardValue(this.deck[this.count].value)
+    this.count++;
+  }
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        //setting cardValues into an array and access them later in the program
-        let cardVal = [];
-        let count = 0;
-        let dealerCount = 0;
-        let playerCount = 0;
+  while(this.count < 3){
+    let elem = document.createElement("img"); 
+    elem.src = this.deck[this.count].image
+    document.querySelector("#dealer-Cards").append(elem)
+    this.dealer += cardValue(this.deck[this.count].value);
+    this.count++;
+  }
 
-        let dealer = document.getElementById("dealer-Cards");
-        let player = document.getElementById("player-Cards");
-        while(dealer.firstChild) {
-          dealer.removeChild(dealer.firstChild);
-        }
-        while(player.firstChild) {
-        player.removeChild(player.firstChild);
-        }
+  document.querySelector("#player-Count").innerText = `Count: ${this.player}`
+  document.querySelector("#dealer-Count").innerText = `Count: ${this.dealer}`
 
-        for(let i = 0; i < 20; i++){
-          cardVal.push((data.cards[i]))
-        }
-        //setting the basic cards for player and dealer
-        while(count < 2){
-          let elem = document.createElement("img"); 
-          elem.src = cardVal[count].image
-          document.querySelector("#player-Cards").append(elem)
-          count++;
-        }
+  //if player gets blackjack
+  if(this.player === 21){
+    while(this.dealer < 21){
+      //call card until blackjack or over
+      this.dealer += cardValue(this.deck[this.count].value);
+      let elem = document.createElement("img"); 
+      elem.src = this.deck[this.count].image;
+      document.querySelector("#dealer-Cards").append(elem);
+      this.count++;
+    }
+    if(this.dealer === 21){
+      this.result("TIE");
+    } else{
+      this.result("WIN")
+    }
+  }
+  }, 350);
+ }
 
-        playerCount = Number(cardValue(cardVal[0].value)) + Number(cardValue(cardVal[1].value));
-        console.log(playerCount);
-        while(count < 3){
-          let elem = document.createElement("img"); 
-          elem.src = cardVal[count].image
-          document.querySelector("#dealer-Cards").append(elem)
-          count++;
-        }
-        dealerCount = Number(cardValue(cardVal[2].value));
-        //add buttons for stopping and taking more cards
-      
-        //if player gets blackjack
-        if(playerCount === 21){
-          while(dealerCount < 21){
-            //call card until blackjack or over
-            dealerCount += Number(cardValue( cardVal[count].value ));
-            let elem = document.createElement("img"); 
-            elem.src = cardVal[count].image
-            document.querySelector("#dealer-Cards").append(elem)
-            count++;
-          }
-          if(dealerCount === 21){
-          } else{
-            win();
-          }
-          
-        }
-        document.querySelector("#player-Count").innerText = `Count: ${playerCount}`
-        document.querySelector("#dealer-Count").innerText = `Count: ${dealerCount}`
-    
-        //Hit function
-        document.querySelector("#hit").addEventListener("click", hitPlayer);
-        function hitPlayer(){
-          if(playerCount < 21){ 
-            playerCount += Number(cardValue(cardVal[count].value));
-            let elem = document.createElement("img"); 
-            elem.src = cardVal[count].image;
-            document.querySelector("#player-Cards").append(elem);
-            document.querySelector("#player-Count").innerText = `Count: ${playerCount}`;
-            count++;
-          }
-          if(playerCount === 21){
-            //call STAY function
-            stay();
-          }
-          if(playerCount > 21){
-            lose();
-          }
-        }
+ hit(){
+   console.log(this.deck)
+   console.log(this.count)
+  if(this.player < 21){ 
+    this.player += cardValue(this.deck[this.count].value);
+    let elem = document.createElement("img"); 
+    elem.src = this.deck[this.count].image;
+    document.querySelector("#player-Cards").append(elem);
+    document.querySelector("#player-Count").innerText = `Count: ${this.player}`;
+    this.count++;
+  }
+  if(this.player === 21){
+    this.stay();
+  }
+  if(this.player > 21){
+    this.result("LOSE")
+  }
+ }
 
-        //Stay function
-        document.querySelector("#stay").addEventListener("click", stay);
+ stay(){
+  while(this.dealer < this.player){
+    //call card until blackjack or over
+    this.dealer += cardValue(this.deck[this.count].value);
+    let elem = document.createElement("img"); 
+    elem.src = this.deck[this.count].image;
+    document.querySelector("#dealer-Cards").append(elem);
+    document.querySelector("#dealer-Count").innerText = `Count: ${this.dealer}`
+    this.count++;
+  }
+  if(this.dealer > 21){
+    this.result("WIN")
+  } else{
+    this.result("LOSE")
+  }
+ }
 
-        function stay(){
-          console.log(playerCount)
-          while(dealerCount < playerCount){
-            //call card until blackjack or over
-            dealerCount += Number(cardValue(cardVal[count].value));
-            let elem = document.createElement("img"); 
-            elem.src = cardVal[count].image
-            document.querySelector("#dealer-Cards").append(elem)
-            document.querySelector("#dealer-Count").innerText = `Count: ${dealerCount}`
-            count++;
-          }
-          if(dealerCount > 21){
-            win();
-          } else{
-            lose();
-          }
-        }
+ result(outcome){
+   if(outcome === "WIN"){
+    this.credits += this.bet;
+    localStorage.setItem("credits", this.credits);
+    document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`
+    alert("WIN")
+   } else if(outcome === "LOSE"){
+    this.credits -= this.bet;
+    localStorage.setItem("credits", this.credits);
+    document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`
+    alert("LOSE")
+   } else {
+     alert("TIE")
+   }
+   this.count = 0;
+   this.dealer = 0;
+   this.player = 0;
+ }
 
-        function win(){
-          console.log("win")
-          let win = Number(localStorage.getItem("credits")) + bet;
-          localStorage.setItem("credits", win);
-          document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`
-        }
-
-        function lose(){
-          console.log("lose")
-          let loss = Number(localStorage.getItem("credits")) - bet;
-            localStorage.setItem("credits", loss);
-            document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`
-        }
-
-        if(data.remaining < 20){
-          fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
-          .then(res => res.json()) // parse response as JSON
-          .then(data => {
-          deckId = data.deck_id
-        })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-        }
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
-
-        
-//if previous values contains 11 then if value goes over 21 set value to 1
-//make an array from where to get the values
-//if pressed stop let dealer get cards instead and then end
-
-function cardValue(val){
+ cardVal(){
   if(val === "ACE"){
     return 11
   }else if (val === "KING"){
@@ -198,6 +166,27 @@ function cardValue(val){
   }else if(val === "JACK"){
     return 10
   }else{
-    return val
+    return Number(val)
   }
+ }
 }
+
+//credits from localstorage
+if(!localStorage.getItem("credits")){
+  localStorage.setItem("credits", 1000)
+}  
+
+let credits = localStorage.getItem("credits");
+document.querySelector("#credits").innerText = `Credits: ${localStorage.getItem("credits")}`;
+
+const blackJack = new Game(credits, [], 0, 0, 0, 100);
+
+//buttons increase/decrease bet
+document.querySelector("#increase").addEventListener("click", blackJack.increaseBet)
+document.querySelector("#decrease").addEventListener("click", blackJack.decreaseBet)
+
+document.querySelector('#dealCards').addEventListener('click', blackJack.renderCards)
+
+document.querySelector("#hit").addEventListener("click", blackJack.hit);
+document.querySelector("#stay").addEventListener("click", blackJack.stay);
+
